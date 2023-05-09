d344add94a3cfefa2119283b1327be4253f6b436
= <neagumario21@yahoo.com>
2023-05-09 21:53:56 +0300

    Change version

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main(int argc, char* argv[])
     TexturePacker tp{};
 
     CLI::App app{};
-    app.set_version_flag("--version", "tpaq v1.2.0");
+    app.set_version_flag("--version", "tpaq v2.0");
     app.add_option("-f,--files", tp.files, "Texture files");
     app.add_option("-d,--directories", tp.directories, 
         "Directories containing texture files (non-recursive by default)");
